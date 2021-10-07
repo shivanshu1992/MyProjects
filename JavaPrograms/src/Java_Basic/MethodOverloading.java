20351ba153aaccb587e0d2df1aafb591e54c7592
shiva <shiva@LAPTOP-ID8GI9CM>
2021-10-08 00:15:00 +0530

    First commit

--- JavaPrograms/src/Java_Basic/MethodOverloading.java
+++ JavaPrograms/src/Java_Basic/MethodOverloading.java
@@ -4,7 +4,24 @@ public class MethodOverloading {
 
 	public static void main(String[] args) {
 		
-
+		MethodOverloading obj1= new MethodOverloading();
+		obj1.sum();
+		obj1.sum(5);
+		obj1.sum("SHIVANSHU");
 	}
 
+	public void sum() {  // method overloading- with same name but diff. arguments
+		System.out.println("This 1st method");
+		
+	}
+public void sum(int a) {
+	
+	System.out.println("This 2nd method");
+	}
+public void sum(String a) {
+	
+	System.out.println("This 3rd method");
 }
+}
+
+
